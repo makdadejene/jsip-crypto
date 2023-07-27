d5b24b80b970c474bb6238389f4f80c2433329e0
Makda Dejene <makdadejene@gmail.com>
2023-07-27 16:28:25 +0000

    graph

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,13 @@
 open! Core
 open! Crypto_src
 open! Crypto_src.Types.Crypto
+(* open Async *)
 
+let () = Command_unix.run Visualize_graph.visualize_command
 let _bitcoin_data = Fetch_data.get_data Bitcoin
-let ethereum_data = Fetch_data.get_data Ethereum
-let _xrp_data = Fetch_data.get_data XRP;;
+let _ethereum_data = Fetch_data.get_data Ethereum
+let _xrp_data = Fetch_data.get_data XRP
 
-print_s [%message (ethereum_data : Types.Total_Data.t)]
+(* let curr_graph = Visualize_graph.curr_graph (); *)
+
+(* print_s [%message (ethereum_data : Types.Total_Data.t)] *)
--- src/visualize_graph.ml
+++ src/visualize_graph.ml
@@ -1,1 +1,22 @@
 open! Core
+module Gp = Gnuplot
+
+let visualize_command =
+  let open Command.Let_syntax in
+  Command.basic
+    ~summary:"test"
+    (let%map_open () = return () in
+     fun () ->
+       let gp = Gp.create () in
+       Gp.plot_many
+         gp
+         ~range:(Gp.XY (-10., 10., -1.5, 1.5))
+         ~output:(Gp.Output.create (`Png "test_output.png"))
+         [ Gp.Series.lines_func "sin(x)" ~title:"Plot a line" ~color:`Blue
+         ; Gp.Series.points_func "cos(x)" ~title:"Plot points" ~color:`Green
+         ];
+       Gp.close gp;
+       (* ignore (input_file : File_path.t); ignore (output_file :
+          File_path.t); *)
+       printf !"Done! Wrote dot file to %s!" "test_output.png")
+;;
