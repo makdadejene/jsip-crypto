799a6ee714cdb62595c35b085a69ad1d4efd2a3a
Daniel Ige <dantomige@gmail.com>
2023-07-27 14:49:12 +0000

    more setyp

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+open! Core
+open! Crypto_src
+open! Crypto_src.Types.Crypto
+
+let _bitcoin_data = Fetch_data.get_data Bitcoin
+let ethereum_data = Fetch_data.get_data Ethereum
+let _xrp_data = Fetch_data.get_data XRP;;
+
+print_s [%message (ethereum_data : Types.Total_Data.t)]
