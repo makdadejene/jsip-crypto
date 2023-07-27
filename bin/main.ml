a3034a5b0f8b8632af6c01ee419452f9677e567f
Makda Dejene <makdadejene@gmail.com>
2023-07-27 20:30:11 +0000

    gnu update

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,11 @@
 open! Core
 open! Crypto_src
 open! Crypto_src.Types.Crypto
-(* open Async *)
+
 
 let () = Command_unix.run Visualize_graph.visualize_command
 let _bitcoin_data = Fetch_data.get_data Bitcoin
 let _ethereum_data = Fetch_data.get_data Ethereum
 let _xrp_data = Fetch_data.get_data XRP
-
-(* let curr_graph = Visualize_graph.curr_graph (); *)
 
 (* print_s [%message (ethereum_data : Types.Total_Data.t)] *)
