b87faed90cbe7b7387b4408a0a1b8b5fe6f2a0ba
Daniel Ige <dantomige@gmail.com>
2023-07-27 20:40:02 +0000

    merge conflict with visualize resolved

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ open! Core
 open! Crypto_src
 open! Crypto_src.Types.Crypto
 
-let () = Command.unix.run Visualize_graph.visualize_command
+let () = Command_unix.run Visualize_graph.visualize_command
 let _bitcoin_data = Fetch_data.get_data Bitcoin
 let _ethereum_data = Fetch_data.get_data Ethereum
 let _xrp_data = Fetch_data.get_data XRP
