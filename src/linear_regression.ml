756c252198e9e0c4dbf885fbd73746e53963973f
Daniel Ige <dantomige@gmail.com>
2023-08-01 14:21:38 +0000

    total data methods

---
+++ src/linear_regression.ml
@@ -0,0 +1,5 @@
+let ordinary_least_squares dataset =
+  let data = List.nth dataset 0 in
+  let data2 = List.nth dataset 0 in
+  snd data, snd data2
+;;
--- src/types.mli
+++ src/types.mli
@@ -40,6 +40,13 @@ module Day_Data : sig
     -> close:float
     -> volume:int
     -> t
+
+  val get_date : t -> Date.t
+  val get_open_ : t -> float
+  val get_high : t -> float
+  val get_low : t -> float
+  val get_close : t -> float
+  val get_volume : t -> int
 end
 
 module Total_Data : sig
@@ -52,4 +59,12 @@ module Total_Data : sig
   val create : Crypto.t -> t
   val add_day_data : t -> Day_Data.t -> unit
   val add_days_data : t -> Day_Data.t list -> unit
+
+  val get_all_dates_prices
+    :  t
+    -> ?market_data_type:string
+    -> unit
+    -> (Date.t * float) list
+
+  val get_all_dates_volume : t -> (Date.t * int) list
 end
