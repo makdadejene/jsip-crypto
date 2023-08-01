598da3f88985738ac8b36993a413fb788c277787
Daniel Ige <dantomige@gmail.com>
2023-08-01 15:16:35 +0000

    linear regression scaffold

--- src/linear_regression.ml
+++ src/linear_regression.ml
@@ -6,7 +6,10 @@ let ordinary_least_squares dataset =
     , Array.of_list (List.map dataset ~f:(fun data_tuple -> snd data_tuple))
     )
   in
-  let x_mean = Owl_base_stats.std x_data in
-  let y_mean = Owl_base_stats.std y_data in 
-  
+  let x_mean, y_mean = Owl_base_stats.mean x_data, Owl_base_stats.mean y_data in 
+  let x_std, y_std = Owl_base_stats.std ~mean:x_mean x_data, Owl_base_stats.std ~mean:y_mean y_data in
+  let r = () in
+  let weight = y_std /. x_std in 
+  let bias = () in 
+  weight, bias
 ;;
