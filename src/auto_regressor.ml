4a7d3af8ecd65b70ee2fe3a6fc9ec9000c75479c
Daniel Ige <dantomige@gmail.com>
2023-08-02 16:19:16 +0000

    continue autoregressor predictor

--- src/auto_regressor.ml
+++ src/auto_regressor.ml
@@ -24,12 +24,16 @@ module Model = struct
   type t =
     { mutable weight : float
     ; mutable bias : float
-    ; mutable sigma : float
     }
+  
+  let create () = {weight=0. ; bias=0.}
+
+  let weight t = t.weight
+  let bias t = t.bias
 
   let linear_regression_function = Linear_regression.ordinary_least_squares
 
-  let update_sigma (data : Types.Total_Data.t) t =
+  (* let update_sigma (data : Types.Total_Data.t) t =
     let day_prices_data = Types.Total_Data.get_all_dates_prices data () in
     let _dates, prices =
       ( Array.of_list
@@ -39,15 +43,17 @@ module Model = struct
     in
     let sigma = Owl_base_stats.std prices in
     t.sigma <- sigma
-  ;;
+  ;; *)
 
-  (* let fit (data : Types.Total_Data.t) t = 
+  (* let fit t (data : Types.Total_Data.t) = 
     let dataset = Types.Total_Data.get_all_dates_prices data () in
-    let dataset = List.map dataset ~f:(fun date, price -> Types. ) 
-    let weight, bias = linear_regression_function dataset in 
+    let dataset = List.map dataset ~f:(fun date, price -> (Types.Date.time_to_unix date), price) in
+    let weight, bias = linear_regression_function dataset in  
     t.weight <- weight; 
     t.bias <- bias
-    ;; *)
+    ;;
+
+  let predict t ~x_val = Linear_regression.predict ~weight:(weight t) ~bias:(bias t) ~x_val  *)
 end
 
 module AutoRegressor = struct
@@ -65,11 +71,16 @@ module AutoRegressor = struct
 
   (* let predict_next_price t = 
     let training_dataset = Types.Total_Data.last_n_days_dataset (dataset t) ~num_of_days:(p t) in
-    let 
-
-    weight, bias = Model.fit
-    t, num_predictions
+    let model = Model.create () in
+    Model.fit model training_dataset;
+    Model.predict model 
 
   let predict_next_n_prices t ~num_predictions =
+    let training_dataset = Types.Total_Data.last_n_days_dataset (dataset t) ~num_of_days:(p t) in
+    for i = 1 to num_predictions do
+      (
+        predict_next_price t
+      )
+    done 
   ;; *)
 end
--- src/simple_model.ml
+++ src/simple_model.ml
@@ -1,2 +1,16 @@
+open! Core
+open! Auto_regressor
+open! Moving_average
+
 let data = Fetch_data.get_day_data Bitcoin
 let calculate_sma _length_of_window = ()
+
+module ArimaModel = struct
+  type t =
+    { ar_model : AutoRegressor.t
+    ; weighted_average : float
+    }
+
+  let next_price t = t ;;
+
+end
