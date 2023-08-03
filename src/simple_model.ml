5849a48ea84675d16b8eaf1ec1f7890cdd20aac1
Daniel Ige <dantomige@gmail.com>
2023-08-03 19:36:04 +0000

    cleaning up arima model

--- src/simple_model.ml
+++ src/simple_model.ml
@@ -7,10 +7,16 @@ let calculate_sma _length_of_window = ()
 
 module ArimaModel = struct
   type t =
-    { ar_model : AutoRegressor.t
-    ; weighted_average : float
+    { mutable ar_model : AutoRegressor.t
+    ; mutable weighted_average : float
+    ; mutable mvg_model : MovingAverageModel.t
+    ; mutable full_dataset : Types.Total_Data.t
     }
 
-  let next_price t = t ;;
+  let update_dateset t dataset = t, dataset ;;
+
+  (* let next_price t = 
+    let ar_model_prediction =  in 
+    let mvg_model_prediction =  in *)
 
 end
